0485f170e4360c0f7f0a94f4694ac8eef20a81bb
Javier Jimenez <javierrcc522@gmail.com>
2018-07-28 10:54:58 -0700

    bare API key doesn't let you do much, because basic security rules from firebase :3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 
 class App extends Component {
+  componentWillMount(){
+    const config = {
+      apiKey: 'AIzaSyBW5-idGWnrnh51J5TBE7C6oMoO4d6sAbc',
+      authDomain: 'manager-fa707.firebaseapp.com',
+      databaseURL: 'https://manager-fa707.firebaseio.com',
+      projectId: 'manager-fa707',
+      storageBucket: 'manager-fa707.appspot.com',
+      messagingSenderId: '98453829472'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return(
       <Provider store={createStore(reducers)}>
