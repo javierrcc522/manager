30cda93fabcb23d979f92cd3ce6ee9544725f9f6
Javier Jimenez <javierrcc522@gmail.com>
2018-07-28 23:26:27 -0700

    added variables types and created immutable state

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,12 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: ''};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
+      // return a brand new object otherwise redux is going to think is the same object
     default:
       return state;
   }
