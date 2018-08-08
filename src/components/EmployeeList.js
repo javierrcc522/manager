237c2a30437d7bbd050ba7270996c1429c354828
Javier Jimenez <javierrcc522@gmail.com>
2018-08-07 20:32:44 -0700

    any key that added to scenes is instantly transcribed onto that actions object

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-Class EmployeeList extends Component {
-    render() {
-        return(
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
-        )
-    }
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+          <Text>Employee List</Text>
+          <Text>Employee List</Text>
+          <Text>Employee List</Text>
+          <Text>Employee List</Text>
+          <Text>Employee List</Text>
+          <Text>Employee List</Text>
+          <Text>Employee List</Text>
+      </View>
+    );
+  }
 }
 
 export default EmployeeList;
