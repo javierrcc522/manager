02b0b2c091046bf3f8569fd137d135288cd26a0a
Javier Jimenez <javierrcc522@gmail.com>
2018-08-07 19:52:33 -0700

    made junk jsx employee list and added another view to the router

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+Class EmployeeList extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Employee List</Text>
+                <Text>Employee List</Text>
+                <Text>Employee List</Text>
+                <Text>Employee List</Text>
+                <Text>Employee List</Text>
+                <Text>Employee List</Text>
+                <Text>Employee List</Text>
+            </View>
+        )
+    }
+}
+
+export default EmployeeList;
