70fe03fc1027d5933b0cdbf0021056ffb9ec30f2
Javier Jimenez <javierrcc522@gmail.com>
2018-08-18 22:05:43 -0700

    removed flex 1, name and phone are loggin but shift isn't

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onButtonPress(){
+    const { name, phone, shift } = this.props;
+
+    this.props.employeeCreate({ name, phone, shift });
+  }
+
   render() {
     return(
       <Card>
@@ -26,9 +32,10 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
+        <CardSection style={{ flexDirection: 'column'}}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
-            style={{flex: 1 }}
+            // style={{ flex: 1 }}
             selectedValue={this.props.shift}
             onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day })}
           >
@@ -43,12 +50,19 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    padding: 20
   }
 }
 
@@ -58,4 +72,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
