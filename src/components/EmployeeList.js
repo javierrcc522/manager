78174124246cc7cf3e47bf864c546c1139aca7be
Javier Jimenez <javierrcc522@gmail.com>
2018-08-27 21:52:27 -0700

    created listItem and added keyExtractor to flatlist, and helper method to render row

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,17 +3,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FlatList, Text } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
   }
 
+  renderRow(employee){
+    return <ListItem employee={employee} />;
+  }
+
   render() {
     return (
       <FlatList
         data={this.props.employees}
-        renderItem={({ item }) => <Text>{item.name}</Text>} // Only for test
+        renderItem={this.renderRow}
+        keyExtractor={employee => employee.uid}
+        // renderItem={({ item }) => <Text>{item.name}</Text>} // Only for test
       />
     );
   }
