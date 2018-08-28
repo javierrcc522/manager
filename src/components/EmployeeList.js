6db5c344baf26532847d597ab3f6654458446622
Javier Jimenez <javierrcc522@gmail.com>
2018-08-27 20:51:06 -0700

    dynamic datasource building

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -6,7 +6,26 @@ import { employeesFetch } from '../actions';
 class EmployeeList extends Component {
   componentWillMount(){
     this.props.employeesFetch();
+    this.createDataSource(this.props);
+
   }
+
+  componentWillReceiveProps(nextProps){
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }){
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.employees);
+  }
+
+
   render() {
     return (
       <View>
