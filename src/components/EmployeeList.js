bb347c73339632560878356444e7e889c8ff6449
Javier Jimenez <javierrcc522@gmail.com>
2018-08-27 21:18:08 -0700

    using flatlist data is updated when this component re-render and  listview is depricated

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,54 +1,27 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
-  componentWillMount(){
+  componentWillMount() {
     this.props.employeesFetch();
-    this.createDataSource(this.props);
-
   }
-
-  componentWillReceiveProps(nextProps){
-    // nextProps are the next set of props that this component
-    // will be rendered with
-    // this.props is still the old set of props
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ employees }){
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(this.props.employees);
-  }
-
 
   render() {
-    console.log(this.props);
     return (
-      <View>
-          <Text>Employee List</Text>
-          <Text>Employee List</Text>
-          <Text>Employee List</Text>
-          <Text>Employee List</Text>
-          <Text>Employee List</Text>
-          <Text>Employee List</Text>
-          <Text>Employee List</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        renderItem={({ item }) => <Text>{item.name}</Text>} // Only for test
+      />
     );
   }
 }
 
-const mapStateToProps =  state => {
-  const employees =_.map(state.employees, (val, uid) => {
-    return { ...val, uid };
-  });
-
-  return { employees }
+const mapStateToProps = (state) => {
+  const employees = _.map(state.employees, (val, uid) => ({ ...val, uid }));
+  return { employees };
 };
 
 export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
